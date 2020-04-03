c77ffb68d6b1d8edd80a593cf5760b45c7d3a231
brad morgan <bkmorgan3@gmail.com>
2020-04-03 10:25:45 -0700

    completed crud with thunks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
-    replaceMe: () => 'Hello!'
-})
+  posts: postsReducer,
+  users: userReducer
+});
